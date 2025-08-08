812e79ff99fcd62a2c62f89f4051961ad40f5f2a
Titir984 <titirsadhu5@gmail.com>
2025-08-08 11:26:47 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String name;
+    private int rollNumber;
+    private double marks;
+
+    public Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+    }
+}
